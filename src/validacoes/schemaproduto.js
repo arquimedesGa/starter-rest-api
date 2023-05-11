0cdd77064c02c1a6447f0149e6f20e18dcee3b4d
ArquimedesGR <gabox2050@gmail.com>
2023-05-11 20:19:47 -0300

    pequena modificacao no joi

--- src/validacoes/schemaproduto.js
+++ src/validacoes/schemaproduto.js
@@ -5,16 +5,18 @@ const schemaproduto = joi.object({
         'string.empty': 'O campo descricao não pode estar vazio',
         'any.required': 'O campo descricao é obrigatório',
     }),
-    quantidade_estoque: joi.number().integer().required().messages({
+    quantidade_estoque: joi.number().min(1).required().messages({
         'number.base': 'O campo quantidade_estoque deve ser um número inteiro',
         'number.empty': 'O campo quantidade_estoque não pode estar vazio',
         'any.required': 'O campo quantidade_estoque é obrigatório',
+        'number.min': 'O campo quantidade_estoque deve ser maior que 0'
     }),
 
-    valor: joi.number().required().messages({
+    valor: joi.number().required().min(1).messages({
         'number.base': 'O campo valor deve ser um número',
         'number.empty': 'O campo valor não pode estar vazio',
         'any.required': 'O campo valor é obrigatório',
+        'number.min': 'O campo valor deve ser maior que 0'
     }),
     categoria_id: joi.number().required().messages({
         'number.base': 'O campo valor deve ser um número',
